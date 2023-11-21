a83f8bf5fb5848775cf972519fe4cc1ac998dc73
jaehnri <joao.henri.cr@gmail.com>
2023-11-20 23:21:46 -0300

    Add prometheus chart

--- internal/client/metrics.go
+++ internal/client/metrics.go
@@ -39,29 +39,3 @@ var (
 		[]string{"clientPodName", "clientNodeName", "serverPodName", "serverNodeName"},
 	)
 )
-
-/*var (
-	dnsLatencyHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "http_dns_request_duration_ms",
-		Help:    "Histogram of DNS requests latency",
-		Buckets: []float64{1, 2, 4, 8, 16, 32, 64, 128, 256, 512},
-	})
-
-	connectionLatencyHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "http_connection_request_duration_ms",
-		Help:    "Histogram of connection establishment latency",
-		Buckets: []float64{1, 2, 4, 8, 16, 32, 64, 128, 256, 512},
-	})
-
-	firstByteLatencyHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "http_first_byte_request_duration_ms",
-		Help:    "Histogram of latency of response first byte",
-		Buckets: []float64{1, 2, 4, 8, 16, 32, 64, 128, 256, 512},
-	})
-
-	httpTotalLatencyHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "http_total_ping_request_duration_ms",
-		Help:    "Histogram of HTTP ping requests latency",
-		Buckets: []float64{1, 2, 4, 8, 16, 32, 64, 128, 256, 512},
-	})
-)*/
